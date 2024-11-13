634ea0fc39e91ee230ea10f35fa52fc1b2593396
Himel Ahmed <himel26723@gmail.com>
2024-11-14 00:24:47 +0600

    3nd commit

---
+++ index2.js
@@ -0,0 +1,12 @@
+function bgGreen(){
+    document.body.style.backgroundColor = 'green'
+};
+
+
+let button = document.getElementById('button').addEventListener('click', function bgOrange(){
+    document.body.style.backgroundColor = 'orange';
+});
+
+
+let buttonYellow = document.getElementById("buttonYellow").addEventListener('click', function buttonYellow(){ document.body.style.backgroundColor = 'yellow'
+});
